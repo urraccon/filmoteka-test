6a31117e288f555dde4f4f7f5130030ba5d42d0d
urraccon <133591634+urraccon@users.noreply.github.com>
2023-12-19 15:51:51 +0200

    moved the firebase configuration to the same folder as the database

--- src/js/db.js
+++ src/js/db.js
@@ -1,5 +1,22 @@
+//firebase configuration
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKYrF2YoqGKSaSU407C9X91DqaZMAg4q4',
+  authDomain: 'filmoteka-urraccon.firebase.com',
+  projectId: 'filmoteka-urraccon',
+  storageBucket: 'filmoteka-urraccon.appspot.com',
+  messagingSenderId: '712511464775',
+  appId: '1:712511464775:web:f28be5eb7fc38469c69862',
+};
+
+const app = initializeApp(firebaseConfig);
+
+// export { app };
+//--------------------------------------------------------------
+
 import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
-import { app } from './firebase_config.js';
+// import { app } from './firebase_config.js';
 // import { watchedMocieList, queuedMovieList } from '';
 // import { DeviceUUID } from 'device-uuid/lib/device-uuid.js';
 // import { v4 as uuid } from 'uuid';
--- src/js/firebase_config.js
+++ src/js/firebase_config.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAKYrF2YoqGKSaSU407C9X91DqaZMAg4q4',
-  authDomain: 'filmoteka-urraccon.firebase.com',
-  projectId: 'filmoteka-urraccon',
-  storageBucket: 'filmoteka-urraccon.appspot.com',
-  messagingSenderId: '712511464775',
-  appId: '1:712511464775:web:f28be5eb7fc38469c69862',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAKYrF2YoqGKSaSU407C9X91DqaZMAg4q4',
+//   authDomain: 'filmoteka-urraccon.firebase.com',
+//   projectId: 'filmoteka-urraccon',
+//   storageBucket: 'filmoteka-urraccon.appspot.com',
+//   messagingSenderId: '712511464775',
+//   appId: '1:712511464775:web:f28be5eb7fc38469c69862',
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export { app };
+// export { app };
